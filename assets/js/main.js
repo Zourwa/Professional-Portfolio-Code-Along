b7e0d7e6d0c58d08d06e260078a2247d37e210f2
Zourwa <zourwa@gmail.com>
2024-12-15 00:54:57 +0200

    upload project to GitHub

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* Show Menu for Mobile */
+
+
+/* Hide Menu for Mobile */
+
+/* Scroll Reveal */
+
+/* Home Section */
+
+/* About Section */
+
+/* Skills Section */
+
+/* Work Section */
+
+
+/* Contact Section */
